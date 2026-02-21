1b522b3f35721a0ce1a910c9e2bd46d4f1189dbc
Rafael Hueb <rafaelhueb@192.168.1.5>
2026-02-21 16:50:11 -0300

    config the pipe

--- init.sh
+++ init.sh
@@ -6,11 +6,15 @@ kind create cluster --config=./kind/kind-config.yaml --name backstage-cluster
 
 kubectl cluster-info --context kind-$CLUSTER_NAME 
 
+echo "Installing Nginx Ingress Controller in the cluster"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
 echo "Installing ArgoCD in the cluster"
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 helm upgrade --install argocd argo/argo-cd -n argocd --create-namespace -f charts/argo/values.yaml
 
-# kubectl rollout restart deployment argocd-server -n argocd
+kubectl rollout restart deployment argocd-server -n argocd
 
 kubectl get all -n argocd
