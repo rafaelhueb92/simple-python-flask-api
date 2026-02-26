35aa4e2b72e9cea7e00355ca45d236eebfe5eb79
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2026-02-26 12:53:46 -0300

    include argo sync

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! This is a simple Flask API.'
 
 @app.route('/json')
 def json_response():
